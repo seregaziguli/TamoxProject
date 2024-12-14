1403cd1287cb6ba5f039441a06f935c01cba4d4d
seregaziguli <seregaziguli@gmail.com>
2024-12-14 19:08:25 +0300

    adding relative imports in chat service

--- backend/chat_service/src/api/deps/chat_deps.py
+++ backend/chat_service/src/api/deps/chat_deps.py
@@ -1,13 +1,11 @@
 from sqlalchemy.ext.asyncio import AsyncSession
-from src.api.deps.session import get_async_session
-from src.repositories.chat_repository import ChatRepository
-from src.services.chat_service import ChatService
+from ...api.deps.session import get_async_session
+from ...repositories.chat_repository import ChatRepository
+from ...services.chat_service import ChatService
 from fastapi import Depends
-from src.utils.logger import logger
-from src.utils.user import verify_user
-from fastapi import WebSocket, Depends, HTTPException
-from src.services.chat_service import ChatService
-from urllib.parse import urlparse, parse_qs
+from ...utils.user import verify_user
+from fastapi import Depends
+from ...services.chat_service import ChatService
 
 async def get_current_user(user: dict = Depends(verify_user)) -> dict:
     return user
--- backend/chat_service/src/api/deps/session.py
+++ backend/chat_service/src/api/deps/session.py
@@ -1,8 +1,6 @@
 from typing import AsyncGenerator
-from sqlalchemy.orm import DeclarativeBase
-from sqlalchemy.ext.asyncio import create_async_engine, async_sessionmaker, AsyncSession
-from src.core.config import settings
-from src.db.session import async_session_maker
+from sqlalchemy.ext.asyncio import AsyncSession
+from ....src.db.session import async_session_maker
 
 async def get_async_session() -> AsyncGenerator[AsyncSession, None]:
     async with async_session_maker() as session:
